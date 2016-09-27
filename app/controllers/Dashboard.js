c4e92470feacb965b875ac9d8fe759ca4110a342
NThanatos <asuramirai@gmail.com>
2016-09-27 23:23:13 +0800

    add tabs to create page

--- app/controllers/Dashboard.js
+++ app/controllers/Dashboard.js
@@ -6,14 +6,45 @@ angular.module('G1.Dashboard', ['ngRoute', 'angularUtils.directives.dirPaginatio
 
         (function initController() {
 
+            $scope.rotated = false;
+            $scope.rotate = function() {
+                $scope.rotated = !$scope.rotated;
+            };
+
+            $scope.NewsRotated = false;
+            $scope.MyCourseRotated = false;
+
+            $scope.hideSection = function() {
+                $scope.MyCourseRotated = !$scope.MyCourseRotated;
+            };
             //Display spinner during data loading
             // $scope.LoadingFalse();
             // $scope.LoadingTrue;
-
 
         })();
 
 //Other Function-------------------------------------------------------------------------------------------------------
 
 
-    }]);    //End of Dashboard controller
+
+    }])
+    .directive("rotateFlip", function() {
+    var first = true;
+    return {
+        restrict: "A",
+        scope: {
+            flag: "=rotateFlip"
+        },
+        link: function(scope, element) {
+            scope.$watch("flag", function() {
+                _toggle(scope, element, !first);
+                first = false;
+            });
+
+            function _toggle(scope, element) {
+                element.toggleClass("rotated", scope.flag);
+            }
+        }
+    }
+    });
+//End of Dashboard controller
