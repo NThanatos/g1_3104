6a23938ba763a17b96a75ab9eb738130cee819a1
NThanatos <asuramirai@gmail.com>
2016-10-04 23:19:55 +0800

    Updated grade page ui

--- app/controllers/Hod.js
+++ app/controllers/Hod.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('G1.Hod', ['ngRoute', 'angularUtils.directives.dirPagination', '720kb.datepicker'])
+angular.module('G1.Hod', ['ngRoute', 'angularUtils.directives.dirPagination', '720kb.datepicker', 'firebase'])
 
     .controller('HodCtrl',  ['$route','$rootScope', '$scope','$location','$http', '$window', '$filter', function ($route,$rootScope, $scope,$location,$http,$window,$filter) {
 
@@ -9,30 +9,61 @@ angular.module('G1.Hod', ['ngRoute', 'angularUtils.directives.dirPagination', '7
             //Display spinner during data loading
             // $scope.LoadingFalse();
             // $scope.LoadingTrue;
+
+             // $scope.Courses=[{title:'Loading', content:'views/courseGrades.html', status:'t'}];
+
             $scope.Courses = [
-                {title:'Programming Fundamentals', content:'views/courseGrades.html'},
-                {title:'Intro to Software Engineering', content:'views/courseGrades.html'},
+                {title:'Intro to Software Engineering', status:'Pending'},
+                {title:'Programming Fundamentals',  status:'Published'}
             ];
 
-            $scope.currentTab = '';
+             var Courses = firebase.database().ref().child("Courses");
+            Courses.on('value', function (datasnapshot) {
+                $scope.Courses = datasnapshot.val();
+                console.log($scope.Courses[1].title);
+                // $scope.Courses.push({
+                //     title: datasnapshot.child("title").val(),
+                //     status: datasnapshot.child("status").val()
+                // })
+            });
 
+
+
+
+
+
+
+            $scope.currentTab = '';
             //Click on tab to change content
             $scope.onClickTab = function (tab) {
-                $scope.currentTab = tab.content;
+                $scope.currentTab = 'views/courseGrades.html';
                 $scope.currentCourse = tab.title;
+                $scope.currentCourseStatus = tab.status;
+
+                if ($scope.currentCourseStatus == 'Pending'){
+                    $scope.SelectedStatus = false;
+                }
+                else {
+                    $scope.SelectedStatus = true;
+                }
 
             };
             $scope.isActiveTab = function(tabContent) {
-                return tabContent == $scope.currentTab;
+                return tabContent == $scope.currentCourse;
             };
+            console.log($scope.Courses[1].title);
+
 
             $scope.Grades = [
                 {Name:'Sam',
-                Grade:'A+'},
+                Grade:'A+',
+                Marks: 90},
                 {Name:'Tom',
-                    Grade:'B+'},
+                    Grade:'B+',
+                    Marks: 83},
                 {Name:'Lee',
-                    Grade:'Fail'}
+                    Grade:'Fail',
+                    Marks: 39}
             ];
             $scope.editGradeForm = true;
             $scope.editGradeFormContent=[];
@@ -41,11 +72,10 @@ angular.module('G1.Hod', ['ngRoute', 'angularUtils.directives.dirPagination', '7
         })();
 
 //Other Function------------------------------------------------------------------------------------------------------
-    $scope.SaveGradeForm = function () {
-        alert("Logic not done!")
-        
+        $scope.SaveGradeForm = function () {
+            alert("Logic not done!")
         $scope.editGradeForm = $scope.editGradeForm === false ? true: false;
-    }
+        }
 
         $scope.PublishGrades = function () {
             alert("Logic not done!")
@@ -55,4 +85,6 @@ angular.module('G1.Hod', ['ngRoute', 'angularUtils.directives.dirPagination', '7
             $scope.editGradeForm = $scope.editGradeForm === false ? true: false;
             $scope.editGradeFormContent = item;
         };
+
+
     }]);    //End of Dashboard controller
